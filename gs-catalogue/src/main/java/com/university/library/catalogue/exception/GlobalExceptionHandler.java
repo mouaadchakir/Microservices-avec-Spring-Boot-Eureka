18cfeb4634b7ed903d59fb899f08b09cff8cb0ee
mouaadchakir <mouaadchk2000@gmail.com>
2025-12-31 16:03:30 +0100

    test

--- gs-catalogue/src/main/java/com/university/library/catalogue/exception/GlobalExceptionHandler.java
+++ gs-catalogue/src/main/java/com/university/library/catalogue/exception/GlobalExceptionHandler.java
@@ -17,7 +17,7 @@ public class GlobalExceptionHandler {
         ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
     }
-
+    
     // Handle global exceptions
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> handleGlobalException(Exception ex, WebRequest request) {
