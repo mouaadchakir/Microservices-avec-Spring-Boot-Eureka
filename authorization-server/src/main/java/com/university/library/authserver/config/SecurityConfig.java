34247e7bda4e05fec263c735522731e55cca801c
mouaadchakir <mouaadchk2000@gmail.com>
2025-12-31 17:12:24 +0100

    Sécuriser les microservices (JWT / OAuth2)

---
+++ authorization-server/src/main/java/com/university/library/authserver/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.university.library.authserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(authorizeRequests ->
+                authorizeRequests.anyRequest().authenticated()
+            )
+            .formLogin(); // Use form login for authentication
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withDefaultPasswordEncoder()
+            .username("user")
+            .password("password")
+            .roles("USER")
+            .build());
+        return manager;
+    }
+}
