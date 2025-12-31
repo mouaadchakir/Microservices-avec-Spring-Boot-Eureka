4f58a391072cbf8bf4ec2d69f5fdb1428070ab85
mouaadchakir <mouaadchk2000@gmail.com>
2025-12-31 18:01:46 +0100

    test prj avec jwt

---
+++ gs-emprunt/src/main/java/com/university/library/emprunt/config/FeignClientInterceptor.java
@@ -0,0 +1,26 @@
+package com.university.library.emprunt.config;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+@Component
+public class FeignClientInterceptor implements RequestInterceptor {
+
+    private static final String AUTHORIZATION_HEADER = "Authorization";
+
+    @Override
+    public void apply(RequestTemplate requestTemplate) {
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        if (attributes != null) {
+            HttpServletRequest request = attributes.getRequest();
+            String authorizationHeader = request.getHeader(AUTHORIZATION_HEADER);
+            if (authorizationHeader != null) {
+                requestTemplate.header(AUTHORIZATION_HEADER, authorizationHeader);
+            }
+        }
+    }
+}
